5a844cc101771b364ee65cae19d86fd6cfabce1f
Jailson Liberato <jailsonliberato@localhost.localdomain>
2018-12-12 16:23:44 -0300

    Subindo arquivos csv para serem processados.

--- Main.py
+++ Main.py
@@ -1,7 +1,17 @@
 # -*- coding: utf-8 -*-
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+import random
+import matplotlib.pyplot as plt
+
 class MainClass:
 
     def __init__(self):
         print("Início programa.")
+        df = pd.read_csv('files/playstore_pre_processed.csv')
+        df.info()
 
 main = MainClass()
